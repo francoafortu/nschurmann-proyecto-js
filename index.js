400d42e2fb31af50da98bd93c33c9dcefefed8c2
Geronimo <gerom2210@gmail.com>
2023-01-16 22:05:48 -0500

    ROW

--- index.js
+++ index.js
@@ -1,14 +1,2 @@
-const fn = () => {
-    const miString = "soy un string!"
-    let b = miString + "muy feliz"
-    console.log(b)
-}
 
-fn()
-
-const clickeado = () => {
-    let a = "chanchito feliz"
-
-    console.log(a)
-}
-print ("holi")
+  
